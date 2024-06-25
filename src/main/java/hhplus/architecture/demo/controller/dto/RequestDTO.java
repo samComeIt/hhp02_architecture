5c8a0ca0f9d5a46908ed48377883d4464767384b
김산해 <sanhae1996@gmail.com>
2024-06-26 00:21:07 +0900

    RequestDTO 작성

--- src/main/java/hhplus/architecture/demo/controller/dto/RequestDTO.java
+++ src/main/java/hhplus/architecture/demo/controller/dto/RequestDTO.java
@@ -1,8 +1,19 @@
 package hhplus.architecture.demo.controller.dto;
 
-public record RequestDTO(
-       Long userId,
-       Long lectureId
-){
+import hhplus.architecture.demo.domain.Enroll;
+import lombok.*;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+public class RequestDTO
+{
+    private Long userId;
+    private Long lectureId;
+
+    @Builder
+    public RequestDTO(Long userId, Long lectureId) {
+        this.userId = userId;
+        this.lectureId = lectureId;
+    }
 }
